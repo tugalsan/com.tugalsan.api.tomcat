35459755d2cf639b73ccf927b9c4c033847103c4
tugalsan karabacak <tugalsan@gmail.com>
2022-12-10 02:31:36 +0300

    watch impl

--- src/main/java/com/tugalsan/api/tomcat/server/TS_TomcatUpdateUtils.java
+++ src/main/java/com/tugalsan/api/tomcat/server/TS_TomcatUpdateUtils.java
@@ -4,7 +4,6 @@ import java.nio.file.*;
 import javax.servlet.*;
 import com.tugalsan.api.file.server.*;
 import com.tugalsan.api.log.server.*;
-import com.tugalsan.api.thread.server.*;
 import com.tugalsan.api.unsafe.client.*;
 
 public class TS_TomcatUpdateUtils {
@@ -15,43 +14,58 @@ public class TS_TomcatUpdateUtils {
         return "pathWarUpdate";
     }
 
-    public static void checkNewWarEvery10Secs(ServletContext ctx, Path pathUpdate) {
-        if (pathUpdate == null) {
-            d.ce("checkNewWarEvery10Secs", "pathUpdate == null");
-            return;
-        }
-        d.cr("checkNewWarEvery10Secs", pathUpdate);
-        TS_ThreadRun.everySeconds(true, 10, () -> {
-            var warNameFull = TS_TomcatPathUtils.getWarNameFull(ctx);
-            var warUpdateFrom = pathUpdate.resolve(warNameFull);
-            var cmdUpdateFrom = Path.of(warUpdateFrom.getParent().toString(), TS_FileUtils.getNameLabel(warUpdateFrom) + ".update");
-            var warUpdateTo = TS_TomcatPathUtils.getPathTomcatWebappsChild(warUpdateFrom.getFileName().toString());
-            d.ci("checkNewWarEvery10Secs", "Thread.warStringFrom/to/Cmd:", warUpdateFrom, warUpdateTo, cmdUpdateFrom);
+    private static void checkNewWar_do(ServletContext ctx, Path pathUpdate) {
+        var warNameFull = TS_TomcatPathUtils.getWarNameFull(ctx);
+        var warUpdateFrom = pathUpdate.resolve(warNameFull);
+        var cmdUpdateFrom = Path.of(warUpdateFrom.getParent().toString(), TS_FileUtils.getNameLabel(warUpdateFrom) + ".update");
+        var warUpdateTo = TS_TomcatPathUtils.getPathTomcatWebappsChild(warUpdateFrom.getFileName().toString());
+        d.ci("checkNewWar_do", "Thread.warStringFrom/to/Cmd:", warUpdateFrom, warUpdateTo, cmdUpdateFrom);
 
-            if (TS_FileUtils.isExistFile(cmdUpdateFrom)) {
-                d.cr("checkNewWarEvery10Secs", "warUpdateCmd detected!");
-                TS_FileUtils.deleteFileIfExists(cmdUpdateFrom);
-                if (TS_FileUtils.isExistFile(warUpdateFrom)) {
-                    TGS_UnSafe.execute(() -> {
-                        d.cr("checkNewWarEvery10Secs", "copying... f/t: " + warUpdateFrom + " / " + warUpdateTo);
-                        TS_FileUtils.copyFile(warUpdateFrom, warUpdateTo, true);
-                    }, e -> d.ce(warNameFull, e));
-                    var modFrom = TS_FileUtils.getTimeLastModified(warUpdateFrom);
-                    var modTo = TS_FileUtils.getTimeLastModified(warUpdateTo);
-                    if (TS_FileUtils.isExistFile(warUpdateTo)) {
-                        if (modFrom.hasEqualDateWith(modTo) && modTo.hasEqualTimeWith(modTo)) {
-                            d.cr("checkNewWarEvery10Secs", "(" + warUpdateTo + ") Successful");
-                            TS_FileUtils.deleteFileIfExists(warUpdateFrom);
-                        } else {
-                            d.ce("checkNewWarEvery10Secs", "Failed", modFrom, modTo);
-                        }
+        if (TS_FileUtils.isExistFile(cmdUpdateFrom)) {
+            d.cr("checkNewWar_do", "warUpdateCmd detected!");
+            TS_FileUtils.deleteFileIfExists(cmdUpdateFrom);
+            if (TS_FileUtils.isExistFile(warUpdateFrom)) {
+                TGS_UnSafe.execute(() -> {
+                    d.cr("checkNewWar_do", "copying... f/t: " + warUpdateFrom + " / " + warUpdateTo);
+                    TS_FileUtils.copyFile(warUpdateFrom, warUpdateTo, true);
+                }, e -> d.ce(warNameFull, e));
+                var modFrom = TS_FileUtils.getTimeLastModified(warUpdateFrom);
+                var modTo = TS_FileUtils.getTimeLastModified(warUpdateTo);
+                if (TS_FileUtils.isExistFile(warUpdateTo)) {
+                    if (modFrom.hasEqualDateWith(modTo) && modTo.hasEqualTimeWith(modTo)) {
+                        d.cr("checkNewWar_do", "(" + warUpdateTo + ") Successful");
+                        TS_FileUtils.deleteFileIfExists(warUpdateFrom);
                     } else {
-                        d.ce("checkNewWarEvery10Secs", "Failed -> warUpdateTo not exists", warUpdateTo);
+                        d.ce("checkNewWar_do", "Failed", modFrom, modTo);
                     }
                 } else {
-                    d.ce("checkNewWarEvery10Secs", "Failed -> warUpdateFrom not detected!", warUpdateFrom);
+                    d.ce("checkNewWar_do", "Failed -> warUpdateTo not exists", warUpdateTo);
                 }
+            } else {
+                d.ce("checkNewWar_do", "Failed -> warUpdateFrom not detected!", warUpdateFrom);
             }
+        }
+    }
+
+    public static void checkNewWar(ServletContext ctx, Path pathUpdate) {
+        if (pathUpdate == null) {
+            d.ce("checkNewWar", "pathUpdate == null");
+            return;
+        }
+        d.cr("checkNewWar", pathUpdate);
+        var warNameFull = TS_TomcatPathUtils.getWarNameFull(ctx);
+        var warUpdateFrom = pathUpdate.resolve(warNameFull);
+        TS_DirectoryUtils.watchCreate(warUpdateFrom, file -> {
+            if (!file.equals(warUpdateFrom)) {
+                return;
+            }
+            checkNewWar_do(ctx, pathUpdate);
+        });
+        TS_DirectoryUtils.watchModify(warUpdateFrom, file -> {
+            if (!file.equals(warUpdateFrom)) {
+                return;
+            }
+            checkNewWar_do(ctx, pathUpdate);
         });
     }
 }
