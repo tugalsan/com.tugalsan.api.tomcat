d62657516fdc286ad2c713e1d65bd76eba4d4db7
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-06-27 12:07:55 +0300

    .

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,6 @@
 module com.tugalsan.api.tomcat {
     requires javax.servlet.api;
-    requires com.tugalsan.api.callable;
+    requires com.tugalsan.api.function;
     requires com.tugalsan.api.log;
     requires com.tugalsan.api.os;
     requires com.tugalsan.api.url;
