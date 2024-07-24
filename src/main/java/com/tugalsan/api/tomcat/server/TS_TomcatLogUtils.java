8e88415dc7c4cd9c6c7c1355cab0056dcbb6c35e
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-07-24 11:49:05 +0300

    Update TS_TomcatLogUtils.java

--- src/main/java/com/tugalsan/api/tomcat/server/TS_TomcatLogUtils.java
+++ src/main/java/com/tugalsan/api/tomcat/server/TS_TomcatLogUtils.java
@@ -1,6 +1,7 @@
 package com.tugalsan.api.tomcat.server;
 
 import com.tugalsan.api.file.server.*;
+import com.tugalsan.api.function.client.TGS_Func;
 import com.tugalsan.api.log.server.*;
 import com.tugalsan.api.thread.server.sync.TS_ThreadSyncTrigger;
 import com.tugalsan.api.thread.server.async.TS_ThreadAsyncScheduled;
@@ -21,7 +22,7 @@ public class TS_TomcatLogUtils {
             TS_DirectoryUtils.createDirectoriesIfNotExists(logFolder);
             var subFiles = TS_DirectoryUtils.subFiles(logFolder, null, false, false);
             (PARALLEL ? subFiles.parallelStream() : subFiles.stream()).forEach(subFile -> {
-                TGS_UnSafe.run(() -> TS_FileUtils.deleteFileIfExists(subFile), e -> TGS_UnSafe.runNothing());
+                TGS_UnSafe.run(() -> TS_FileUtils.deleteFileIfExists(subFile), e -> TGS_Func.empty.run());
             });
         });
     }
